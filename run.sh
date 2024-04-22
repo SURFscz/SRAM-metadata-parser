08265d884c6d112ab4aa1371b2243ec3c1549e7e
Martin van Es <martin@mrvanes.com>
2024-04-22 09:51:38 +0200

    Add parse function

--- run.sh
+++ run.sh
@@ -9,42 +9,41 @@ if [ ! -f cert/signing.key ]; then
     openssl req -x509 -newkey rsa:4096 -keyout cert/signing.key -out cert/signing.pem -sha256 -days 365 -nodes -subj "/CN=Signing"
 fi
 
-./pyff-lite.py config/test.yml tmp/test_parsed.xml
-xalan -in tmp/test_parsed.xml -xsl xslt/cleanup_test.xslt > tmp/test_parsed_clean.xml
-xmlsec1 sign --pubkey-cert-pem cert/signing.pem --privkey-pem cert/signing.key --output out/test_signed.xml tmp/test_parsed_clean.xml
-xmlsec1 verify --pubkey-cert-pem cert/signing.pem out/test_signed.xml
+parse() {
+    PARSED=$(mktemp --tmpdir=tmp/)
+    CLEANED=$(mktemp --tmpdir=tmp/)
+    SIGNED=out/${1}_signed.xml
+    trap 'rm -f -- tmp/tmp.*' EXIT
+
+    ./venv/bin/python pyff-lite.py config/${1}.yml $PARSED
+    xalan -in $PARSED -xsl xslt/${2} > $CLEANED
+    xmlsec1 sign --pubkey-cert-pem cert/signing.pem --privkey-pem cert/signing.key --output $SIGNED $CLEANED
+    xmlsec1 verify --pubkey-cert-pem cert/signing.pem $SIGNED
+    rm -f -- tmp/tmp.*
+}
+
+# Test
+parse test cleanup_test.xslt
 
 # IDP's
 wget https://metadata.test.surfconext.nl/idps-metadata.xml -O tmp/idps-metadata.xml
 xmlsec1 verify --pubkey-cert-pem cert/surfconext.crt --id-attr:ID EntitiesDescriptor  tmp/idps-metadata.xml
-./pyff-lite.py config/idps.yml tmp/idps-metadata_parsed.xml
-xalan -in tmp/idps-metadata_parsed.xml -xsl xslt/cleanup_idps.xslt > tmp/idps-metadata_parsed_clean.xml
-xmlsec1 sign --pubkey-cert-pem cert/signing.pem --privkey-pem cert/signing.key --output out/idps-metadata_signed.xml tmp/idps-metadata_parsed_clean.xml
-xmlsec1 verify --pubkey-cert-pem cert/signing.pem out/idps-metadata_signed.xml
+parse idps-metadata cleanup_idps.xslt
 
 # Backend
 wget https://proxy.acc.sram.eduteams.org/metadata/backend.xml -O tmp/backend.xml
 # xmlsec1 verify --pubkey-cert-pem cert/backend.crt tmp/backend.xml
-./pyff-lite.py config/backend.yml tmp/backend_parsed.xml
-xalan -in tmp/backend_parsed.xml -xsl xslt/cleanup_proxy.xslt > tmp/backend_parsed_clean.xml
-xmlsec1 sign --pubkey-cert-pem cert/signing.pem --privkey-pem cert/signing.key --output out/backend_signed.xml tmp/backend_parsed_clean.xml
-xmlsec1 verify --pubkey-cert-pem cert/signing.pem out/backend_signed.xml
+parse backend cleanup_proxy.xslt
 
 # Frontend
 wget https://proxy.acc.sram.eduteams.org/metadata/frontend.xml -O tmp/frontend.xml
 # xmlsec1 verify --pubkey-cert-pem cert/frontend.crt tmp/frontend.xml
-./pyff-lite.py config/frontend.yml tmp/frontend_parsed.xml
-xalan -in tmp/frontend_parsed.xml -xsl xslt/cleanup_proxy.xslt > tmp/frontend_parsed_clean.xml
-xmlsec1 sign --pubkey-cert-pem cert/signing.pem --privkey-pem cert/signing.key --output out/frontend_signed.xml tmp/frontend_parsed_clean.xml
-xmlsec1 verify --pubkey-cert-pem cert/signing.pem out/frontend_signed.xml
+parse frontend cleanup_proxy.xslt
 
 # Edugain
 if [ ! -f tmp/edugain.xml ]; then
      wget https://mds.edugain.org/edugain-v2.xml -O tmp/edugain.xml
+    xmlsec1 verify --pubkey-cert-pem cert/mds-v2.cer --id-attr:ID EntitiesDescriptor tmp/edugain.xml
 fi
-xmlsec1 verify --pubkey-cert-pem cert/mds-v2.cer --id-attr:ID EntitiesDescriptor tmp/edugain.xml
-./pyff-lite.py config/edugain.yml tmp/edugain_parsed.xml
-xalan -in tmp/edugain_parsed.xml -xsl xslt/cleanup_edugain.xslt > tmp/edugain_parsed_clean.xml
-xmlsec1 sign --pubkey-cert-pem cert/signing.pem --privkey-pem cert/signing.key --output out/edugain_signed.xml tmp/edugain_parsed_clean.xml
-xmlsec1 verify --pubkey-cert-pem cert/signing.pem out/edugain_signed.xml
+parse edugain cleanup_edugain.xslt
 
